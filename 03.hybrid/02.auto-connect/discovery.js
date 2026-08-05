675f063167323e90af1ab9f3907f986a26c9bebf
Shivam Chaudhary <skc722768@gmail.com>
2026-08-05 21:16:46 +0530

    Add UDP discovery and server implementation for device connection

---
+++ 03.hybrid/02.auto-connect/discovery.js
@@ -0,0 +1,70 @@
+import dgram from "node:dgram";
+import crypto from "node:crypto";
+
+const socket = dgram.createSocket("udp4");
+
+const sessionId = crypto.randomUUID();
+
+socket.on("listening", () => {
+  socket.setBroadcast(true);
+
+  const msg = JSON.stringify({
+    type:"DISCOVER_RESPONSE",
+    sessionId,
+});
+
+  setInterval(() => {
+  socket.send(msg, 4242, "255.255.255.255", (err) => {
+    if (err) {
+      console.error(" Socket Error: ", err);
+      socket.close();
+    }
+  });
+
+  },3000);
+
+  const address = socket.address();
+  console.log(`UDP server running ${address.address}:${address.port}`);
+});
+
+const discoveredDevices = new Map();
+
+socket.on("message", (msg, rinfo) => {
+  try {
+    const data = JSON.parse(msg);
+
+    if (data.sessionId == sessionId) {
+      return;
+    }
+
+    discoveredDevices.set(data.sessionId, {
+      sessionId: data.sessionId,
+      udpPort: rinfo.port,
+      udpAddress: rinfo.address,
+      udpFamily: rinfo.family,
+      lastSeen: new Date(),
+    });
+
+
+  } catch (err) {
+    console.log("Invalid data : ", err.message);
+  }
+});
+
+setInterval(() => {
+  const now = new Date();
+
+  for (const [id, device] of discoveredDevices) {
+    if (now - device.lastSeen > 6000) {
+      console.log(`${device.sessionId} offline`);
+      discoveredDevices.delete(id);
+    }
+  }
+}, 3000);
+
+socket.on("error", (err) => {
+  console.log("UDP socket error : ", err);
+  socket.close();
+});
+
+socket.bind(4242);
